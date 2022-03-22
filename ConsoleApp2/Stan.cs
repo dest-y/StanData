3db2a761fe7dc529f817c4957298b971f45205b0
morm934 <morm934@gmail.com>
2022-03-23 00:19:26 +0300

    init commit

---
+++ ConsoleApp2/Stan.cs
@@ -0,0 +1,34 @@
+﻿using Sharp7;
+internal class Stan
+{
+
+    private string Name;
+    private string IpAddress;
+    private int Counter;
+    private bool Status;
+    private bool WireBreak = false;
+    private bool DrawingChange = false;
+
+    public Stan(string name, string ipAddress, int counter = 0, bool status = false)
+    {
+        Name = name;
+        IpAddress = ipAddress;
+        Counter = counter;
+        Status = status;
+    }
+
+    public void GetName()
+    {
+        Console.WriteLine("Имя Стана: {0}", Name);
+    }
+
+    //public int getCounter()
+    //{
+    //    var buffer = new byte[4];
+    //    int readResult = client.DBRead(66, 210, 4, buffer);
+    //    int tmp = buffer[0] * 16777216 + buffer[1] * 65536 + buffer[2] * 256 + buffer[3];
+    //    Console.WriteLine("Стан : {1}" + "Величина счетчика : {0}", tmp, Name);
+
+    //    return tmp;
+    //}
+}
---
+++ ConsoleApp2/stan789.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharp7;
+
+namespace ConsoleApp2
+{
+    public class Stan789
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private string Name;
+        private string IpAddress;
+        private int Counter;
+        private bool Status;
+        private bool WireBreak;
+        private bool DrawingChange;
+        private bool CointerErase = false;
+        S7Client client = new();
+
+        public Stan789(string name, string ipAddress, int counter = 0, bool status = false)
+        {
+            Name = name;
+            IpAddress = ipAddress;
+            Counter = counter;
+            Status = status;
+        }
+
+        public void getData() 
+        {
+            var dbuffer = new byte[4];
+            var buffer = new byte[2];
+            int ireadResult;
+            int breadResult;
+
+            client.SetConnectionType(S7Client.CONNTYPE_BASIC);
+
+            int connectionResult = client.ConnectTo(IpAddress, 0, 3); //10-11 стан 
+            if (connectionResult == 0)
+            {
+                Console.WriteLine("Connection OK");
+                PrintName();
+            }
+
+            Console.WriteLine("Начало данных");
+            ireadResult = client.DBRead(66, 210, 4, dbuffer);
+            int tmp = dbuffer[0] * 16777216 + dbuffer[1] * 65536 + dbuffer[2] * 256 + dbuffer[3];
+            Counter = tmp;
+
+            breadResult = client.DBRead(251, 8, 2, buffer);
+            bool D01DB251DBX = S7.GetBitAt(buffer, 0, 1);
+            DrawingChange = D01DB251DBX;
+
+            breadResult = client.MBRead(4, 4, dbuffer);
+            bool mb4_0 = S7.GetBitAt(buffer, 0, 3);
+            bool mb7_0 = S7.GetBitAt(buffer, 0, 0);
+            
+            Status = mb4_0;
+            
+
+            
+            CointerErase = mb7_0;
+
+            breadResult = client.MBRead(142, 2, buffer);
+            bool mb142_0 = S7.GetBitAt(buffer, 0, 0);
+            
+            
+            WireBreak = mb142_0;
+
+            Console.WriteLine("Длина счетчика : " + Counter);
+            Console.Write("Замена Волок: ");
+            Console.WriteLine(DrawingChange);
+            Console.Write("Стан в работе(если false значит ON): ");
+            Console.WriteLine(Status);
+            Console.Write("Сброс счетчика: ");
+            Console.WriteLine(CointerErase);
+            Console.Write("Обрыв проволоки: ");
+            Console.WriteLine(WireBreak);
+            Console.WriteLine("Конец данных");
+            Logger.Info("Имя стана: {0}; Длина счетчика: {1}; Стан В работе: {2}; Обрыв: {3}; Смена волок: {4};", Name, Counter, !Status, WireBreak, CointerErase);
+
+            client.Disconnect();
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Имя Стана: {0}", Name);
+        }
+    }
+}
